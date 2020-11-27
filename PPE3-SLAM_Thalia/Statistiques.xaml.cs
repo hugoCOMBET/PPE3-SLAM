2b472dbcafc7a34560a62eff777d3cef2d459b11
thalia sahed <thalia.sahed@saintmichelannecy.fr>
2020-11-27 16:47:17 +0100

    essaie binding

---
+++ PPE3-SLAM_Thalia/Statistiques.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppDirecteur_PPE3
+{
+    /// <summary>
+    /// Logique d'interaction pour Statistiques.xaml
+    /// </summary>
+    public partial class Statistiques : Window
+    {
+        public Statistiques()
+        {
+            InitializeComponent();
+            //on associe la grid principal mainGrid du XAML en initialisant son DataContext avec un objet viewModelFromage
+            //mainGrid.DataContext = new viewModel.viewModelStatistiques();
+        }
+    }
+}
---
+++ PPE3-SLAM_Thalia/viewModel/viewModelStatistiques.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDirecteur_PPE3.viewModel
+{
+    class viewModelStatistiques
+    {
+    }
+}
