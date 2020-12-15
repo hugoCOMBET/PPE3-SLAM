6c473b4a224ec5a3bf877d28d0026a3bf3efa7e0
thalia sahed <thalia.sahed@saintmichelannecy.fr>
2020-12-15 22:55:35 +0100

    active et selected avis

--- PPE3-SLAM_Thalia/viewModel/viewModelAvis.cs
+++ PPE3-SLAM_Thalia/viewModel/viewModelAvis.cs
@@ -7,16 +7,22 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System;
 
 namespace AppDirecteur_PPE3.viewModel
 {
-    class viewModelAvis
+    class viewModelAvis : viewModelBase
     {
         DAOavis vmDaoAvis;
         DAOclients vmDaoClient;
         DAOsalles vmDaoSalle;
         DAOtheme vmDaoTheme;
         daoReservation vmDaoReservation;
+        private salle activeSalle = new salle();
+        private theme activeTheme = new theme();
+        private avis selectedAvis = new avis();
         private ObservableCollection<avis> listAvis;
         private ObservableCollection<theme> listTheme;
         private ObservableCollection<salle> listSalle;
@@ -58,22 +64,56 @@ namespace AppDirecteur_PPE3.viewModel
             }
         }
 
-        //public avis SelectedAvis
-        //{
-        //    //get => selectedFromage;
-        //    //set
-        //    //{
-        //    //    if (selectedFromage != value)
-        //    //    {
-        //    //        selectedFromage = value;
-        //    //        //création d'un évènement si la propriété Name (bindée dans le XAML) change
-        //    //        OnPropertyChanged("SelectedFromage");
-        //    //        if (selectedFromage != null)
-        //    //        {
-        //    //            ActiveFromage = selectedFromage;
-        //    //        }
-        //    //    }
-        //    //}
-        //}
+
+        public avis SelectedAvis
+        {
+            get => selectedAvis;
+            set
+            {
+                if (selectedAvis != value)
+                {
+                    selectedAvis = value;
+                    //création d'un évènement si la propriété Name (bindée dans le XAML) change
+                    OnPropertyChanged("SelectedAvis");
+                    if (selectedAvis != null)
+                    {
+                        ActiveTheme = selectedAvis;
+                        ActiveSalle = selectedAvis;
+                    }
+                }
+            }
+        }
+
+        public theme ActiveTheme
+        {
+            get => activeTheme;
+            set
+            {
+                if (activeTheme != value)
+                {
+                    activeTheme = value;
+                    //création d'un évènement si la propriété Name (bindée dans le XAML) change
+                    OnPropertyChanged("Name");
+                    OnPropertyChanged("Origin");
+                    OnPropertyChanged("Creation");
+                }
+            }
+        }
+
+        public salle ActiveSalle
+        {
+            get => activeSalle;
+            set
+            {
+                if (activeSalle != value)
+                {
+                    activeSalle = value;
+                    //création d'un évènement si la propriété Name (bindée dans le XAML) change
+                    OnPropertyChanged("Name");
+                    OnPropertyChanged("Origin");
+                    OnPropertyChanged("Creation");
+                }
+            }
+        }
     }
 }
